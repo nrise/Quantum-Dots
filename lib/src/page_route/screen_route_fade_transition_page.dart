d00470691eae9dbc86ec32ae10726e9c79181775
송진우 <sugwer7464@gmail.com>
2025-02-06 16:15:34 +0900

    add white background

--- lib/src/page_route/screen_route_fade_transition_page.dart
+++ lib/src/page_route/screen_route_fade_transition_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quantum_dots/qds_foundation.dart';
 import 'package:quantum_dots/src/page_route/builder/wippy_transition_page.dart';
 
 class ScreenRouteFadeTransitionPage<T> extends WippyTransitionPage<T> {
@@ -21,12 +22,21 @@ class ScreenRouteFadeTransitionPage<T> extends WippyTransitionPage<T> {
       curve: Curves.easeInOut,
     );
 
-    return FadeTransition(
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: wippyWhite,
+        ),
+        FadeTransition(
           opacity: Tween<double>(
             begin: 0.0,
             end: 1.0,
           ).animate(curvedAnimation),
           child: child,
-        );
+        ),
+      ],
+    );
   }
 }
